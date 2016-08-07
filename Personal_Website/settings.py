bd4c56782fc0d8e7621f48dda58e1bc9a04c4b50
Evan <eklein217@gmail.com>
2016-08-07 15:55:30 -0400

    debug = false

--- Personal_Website/settings.py
+++ Personal_Website/settings.py
@@ -138,7 +138,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'))
+STATICFILES_DIRS = [(os.path.join(PROJECT_ROOT, 'static')]
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
