ab2e2b35e1f5452a38cfd0bd283c5ea8384a747c
Evan <eklein217@gmail.com>
2016-08-07 12:16:17 -0400

    try

--- Personal_Website/settings.py
+++ Personal_Website/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = ''
+STATIC_ROOT = BASE_DIR
 
 STATIC_URL = '/static/'
 
