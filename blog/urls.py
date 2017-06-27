cb6ec2235b0efe8073ea10350ab5c65f5647e85a
hasherr <klein57@purdue.edu>
2017-06-26 22:01:55 -0400

    pretty

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.blog, name='blog'),
-    url(r'^Steganography', views.steganography, name='Steganography'),
-    url(r'^Recording', views.recording, name='Recording')
-]
